db090a3b7d3053b830cdc3674a01c35c7ac5eaf1
09Rolo <106391463+09Rolo@users.noreply.github.com>
2024-10-26 18:28:07 +0200

    Add files via upload

--- securelogin.py
+++ securelogin.py
@@ -21,7 +21,7 @@ while 1 == 1:
         print("L")
         if os.path.exists(f'{dict}/imhere'):
             iconsetted = False
-            os.rmdir(f'{dict}/imhere')
+            shutil.rmtree(f'{dict}/imhere', ignore_errors=True)
     else:
         if dict == None:
             print("Nincs asztal hely megadva. A directory fileba másold be az asztal helyét (pl: C:/-Rolo-/Asztal)")
@@ -30,16 +30,19 @@ while 1 == 1:
 
             if os.path.exists(f'{dict}/imhere'):
                 if (iconsetted == False):
-                    iconsetted = True
+                    try:
+                        iconsetted = True
 
-                    shutil.copy(f"{os.getcwd()}/ficon.ico", f"{dict}/imhere")
+                        shutil.copy(f"{os.getcwd()}/ficon.ico", f"{dict}/imhere")
 
-                    with open(f"{dict}/imhere/desktop.ini", "w") as file:
-                        file.write(f"[.ShellClassInfo]\nIconFile=ficon.ico\nIconResource=" + dict.replace("/", "\\") + "\\imhere\\ficon.ico,0")
-                        file.close()
+                        with open(f"{dict}/imhere/desktop.ini", "w") as file:
+                            file.write(f"[.ShellClassInfo]\nIconFile=ficon.ico\nIconResource=" + dict.replace("/", "\\") + "\\imhere\\ficon.ico,0")
+                            file.close()
 
-                    os.system(f'attrib +s +r "{dict}/imhere"')
-                    os.system(f'attrib +s +h "{dict}/imhere/desktop.ini"')
+                        os.system(f'attrib +s "{dict}/imhere"')
+                        os.system(f'attrib +s +h "{dict}/imhere/desktop.ini"')
+                    except:
+                        print("")
 
             else:
                 ctypes.windll.user32.LockWorkStation()
